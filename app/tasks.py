dec351d2f0a2efceea46c533b32dacc3fb4e4ec1
carlosribas <caduribas@gmail.com>
2024-06-18 15:02:07 +0100

    Fetch data from database in batches

--- app/tasks.py
+++ app/tasks.py
@@ -2,7 +2,7 @@ import json
 import os
 import requests
 
-from pydantic import BaseModel, ValidationError
+from pydantic import BaseModel
 
 from .celery import celery_app
 from .database import fetch_data_from_db
@@ -16,25 +16,36 @@ class APIData(BaseModel):
 
 @celery_app.task(bind=True)
 def fetch_data_from_search_index(self, api_url: str):
-    # set a custom initial state to help validate the existence of a task_id
-    self.update_state(state="SUBMITTED")
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        try:
+    self.update_state(state="SUBMITTED")  # set a custom initial state
+    search_position = "0"  # initial search position
+    ids = []
+
+    while True:
+        response = requests.get(api_url + f"&searchposition={search_position}")
+        if response.status_code == 200:
             data = response.json()
-            extracted_ids = [entry["id"] for entry in data["entries"]]
-            additional_data = fetch_data_from_db(extracted_ids)
-            file_path = save_ids_to_file(additional_data, self.request.id)
-            return file_path
-        except (ValidationError, KeyError) as e:
-            raise Exception(f"Error processing data: {e}")
-    else:
-        raise Exception("Failed to fetch data")
+            entries = data.get("entries", [])
+            if not entries:
+                break  # there is no more data to fetch
+            ids.extend(entry["id"] for entry in entries)
+            search_position = data.get("searchPosition")  # next position
+            if not search_position:
+                break
+        else:
+            raise Exception("Failed to fetch data")
 
+    # fetch data from database in batches
+    batch_size = 1000
+    db_data = []
+    for i in range(0, len(ids), batch_size):
+        batch_ids = ids[i:i + batch_size]
+        batch_data = fetch_data_from_db(batch_ids)
+        db_data.extend(batch_data)
 
-def save_ids_to_file(data, task_id):
-    file_path = f"/srv/results/{task_id}.json"
+    # save the data to a file
+    file_path = f"/srv/results/{self.request.id}.json"
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    with open(file_path, "w") as f:
-        json.dump(data, f)
+    with open(file_path, "w") as file:
+        json.dump(db_data, file, default=str)
+
     return file_path
