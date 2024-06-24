9e4180d83e8d056b0c1162f2dd05a16804619d29
carlosribas <caduribas@gmail.com>
2024-06-24 11:32:44 +0100

    Add path to esl_binary

--- app/config.py
+++ app/config.py
@@ -6,6 +6,8 @@ class Settings(BaseSettings):
     celery_result_backend: str = "redis://redis:6379/0"
     database: str = "postgresql+asyncpg://reader:NWDMCE5xdipIjRrp@hh-pgsql-public.ebi.ac.uk:5432/pfmegrnargs"
     dev: bool = False
+    esl_binary: str = "/srv/local/infernal-1.1.5/bin/esl-sfetch"
+    fasta: str = "/srv/rnacentral-export/app/examples.fasta"
 
     class Config:
         env_file = ".env"
