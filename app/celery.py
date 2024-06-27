65d286d66f04f83c79dc32354c65e4b5de312623
carlosribas <caduribas@gmail.com>
2024-06-27 12:48:28 +0100

    Use default options again

--- app/celery.py
+++ app/celery.py
@@ -17,11 +17,4 @@ celery_app.conf.update(
     result_serializer="json",
     timezone="UTC",
     enable_utc=True,
-    task_acks_late=True,
-    task_reject_on_worker_lost=True,  # task is re-queued if worker crashes
-    task_default_retry_delay=60,  # retry after 60 sec
-    task_max_retries=3,  # retry up to 3 times
-    task_soft_time_limit=300,  # soft time limit of 5 minutes
-    task_time_limit=360,  # hard time limit of 6 minutes
-    worker_max_tasks_per_child=100,  # restart worker after 100 tasks
 )
