947d4d027b238349bb32bf2897b77b8b3128b386
carlosribas <caduribas@gmail.com>
2024-07-18 14:05:13 +0100

    Increase chunk size

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-import aiofiles
 import os
 
 from fastapi import FastAPI, HTTPException
@@ -54,21 +53,21 @@ def fetch_data(request: APIRequest):
 
 
 @app.get("/download/{task_id}/fasta")
-async def download_fasta(task_id: str):
-    return await download_file(task_id, "fasta")
+def download_fasta(task_id: str):
+    return download_file(task_id, "fasta")
 
 
 @app.get("/download/{task_id}/txt")
-async def download_txt(task_id: str):
-    return await download_file(task_id, "txt")
+def download_txt(task_id: str):
+    return download_file(task_id, "txt")
 
 
 @app.get("/download/{task_id}/json")
-async def download_json(task_id: str):
-    return await download_file(task_id, "json")
+def download_json(task_id: str):
+    return download_file(task_id, "json")
 
 
-async def download_file(task_id: str, data_type: str):
+def download_file(task_id: str, data_type: str):
     result = fetch_data_from_search_index.AsyncResult(task_id)
     if result.state == "PENDING":
         # a valid task_id will never be PENDING, as we are changing the initial
@@ -101,7 +100,7 @@ async def download_file(task_id: str, data_type: str):
         file_path = f"/srv/results/{task_id}.{file_extension}"
         if os.path.exists(file_path):
             logger.info(f"Showing results for Task ID: {task_id}")
-            return await stream_file(file_path, f"{task_id}.{file_extension}")
+            return stream_file(file_path, f"{task_id}.{file_extension}")
         else:
             logger.error(f"Results file could not be found: {task_id}")
             raise HTTPException(status_code=404, detail="File not found")
@@ -122,10 +121,11 @@ async def download_file(task_id: str, data_type: str):
         raise HTTPException(status_code=202, detail="Task is still processing")
 
 
-async def stream_file(file_path: str, filename: str):
-    async def read_file():
-        async with aiofiles.open(file_path, "rb") as file:
-            while chunk := await file.read(16384):  # Read in 16MB chunks
+def stream_file(file_path: str, filename: str):
+    def read_file():
+        chunk_size = 1024 * 1024 * 16  # 16MB
+        with open(file_path, "rb") as file:
+            while chunk := file.read(chunk_size):
                 yield chunk
 
     file_size = os.path.getsize(file_path)
